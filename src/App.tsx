a131a6c7eaa3f6f4a8d5843c5a5beff4220968f0
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-11-28 11:44:29 +0200

    Remove unused files and add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from '@material-ui/core/Button'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Hello React World!</h1>
+            <Button variant='contained' color='primary'>Material UI Enabled</Button>
+        </div>
+    );
 }
 
 export default App;
