6c6ae71ff7af34b9fd798a67f33ed038152e17a3
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-12-08 13:02:44 +0200

    Add props to OverviewDetailItem

--- src/components/Forecast/OverviewDetailItem/OverviewDetailItem.tsx
+++ src/components/Forecast/OverviewDetailItem/OverviewDetailItem.tsx
@@ -2,45 +2,51 @@ import React from 'react';
 import { Grid, Typography, Paper } from '@material-ui/core';
 import DetailItem from '../../DetailWeather/DetailItem/DetailItem';
 import DetailItemType from '../../DetailWeather/DetailItem/DetailItemTypes';
+import { IForecastWeather } from '../../../store/Weather/models/Weather';
+import { UnixUTCToHourMinuteString } from '../../../utils/DateConverter';
 
-const OverviewDetailItem = () => {
+interface OverviewDetailItemProps {
+    forecast: IForecastWeather;
+}
+
+const OverviewDetailItem: React.FC<OverviewDetailItemProps> = (props) => {
     return(
         <Grid container spacing={2}>
             <Grid item xs={2}/>
             <Grid item xs={10}>
                 
                 <div style={{textAlign: 'left', marginLeft: '12px'}}>
-                    <Typography variant='subtitle1'>Weather Description</Typography>
+                    <Typography variant='subtitle1'>{props.forecast.weather[0].description}</Typography>
 
                     <Grid container spacing={2} style={{marginTop: '15px'}}>
                         <Grid item xs={6} md={4} lg={3}>
                             <Paper variant='outlined' >
-                                <DetailItem itemType={DetailItemType.WIND} value={15}/>
+                                <DetailItem itemType={DetailItemType.WIND} value={props.forecast.windSpeed}/>
                             </Paper>
                         </Grid>
                         <Grid item xs={6} md={4} lg={3}>
                             <Paper variant='outlined' >
-                                <DetailItem itemType={DetailItemType.CLOUD_COVER} value={75}/>
+                                <DetailItem itemType={DetailItemType.CLOUD_COVER} value={props.forecast.clouds}/>
                             </Paper>
                         </Grid>
                         <Grid item xs={6} md={4} lg={3}>
                             <Paper variant='outlined' >
-                                <DetailItem itemType={DetailItemType.UV_INDEX} value={2}/>
+                                <DetailItem itemType={DetailItemType.UV_INDEX} value={props.forecast.uvi}/>
                             </Paper>
                         </Grid>
                         <Grid item xs={6} md={4} lg={3}>
                             <Paper variant='outlined' >
-                                <DetailItem itemType={DetailItemType.PRESSURE} value={1023}/>
+                                <DetailItem itemType={DetailItemType.PRESSURE} value={props.forecast.pressure}/>
                             </Paper>
                         </Grid>
                         <Grid item xs={6} md={4} lg={3}>
                             <Paper variant='outlined' >
-                                <DetailItem itemType={DetailItemType.SUNRISE} value={'06:52'}/>
+                                <DetailItem itemType={DetailItemType.SUNRISE} value={UnixUTCToHourMinuteString(props.forecast.sunrise)}/>
                             </Paper>
                         </Grid>
                         <Grid item xs={6} md={4} lg={3}>
                             <Paper variant='outlined' >
-                                <DetailItem itemType={DetailItemType.SUNSET} value={'17:35'}/>
+                                <DetailItem itemType={DetailItemType.SUNSET} value={UnixUTCToHourMinuteString(props.forecast.sunset)}/>
                             </Paper>
                         </Grid>
                     </Grid>
