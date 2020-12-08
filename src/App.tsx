b26b6535d6177207345a1b52a9a0a6f4b910d473
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-12-08 18:31:31 +0200

    Add routes to main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 import CurrentWeatherContainer from "./containers/CurrentWeatherContainer/CurrentWeatherContainer";
 import ForecastContainer from "./containers/ForecastContainer/ForecastContainer";
 
 function App() {
     return (
         <React.Fragment>
-            <CurrentWeatherContainer />
-            <ForecastContainer />
+            <h1>Hello React Weather</h1>
+            <Switch>
+                <Route path='/forecast' component={ForecastContainer} />
+                <Route path='/' component={CurrentWeatherContainer} />
+                <Redirect to='/' />
+            </Switch>
         </React.Fragment>
     );
 }
 
-export default App;
+export default withRouter(App);
