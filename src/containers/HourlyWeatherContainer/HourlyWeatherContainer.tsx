db8ec25c3aed4f4ca30e2a014a217bcf4fb90a4b
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-12-09 13:30:38 +0200

    Use local state to manage chart type

--- src/containers/HourlyWeatherContainer/HourlyWeatherContainer.tsx
+++ src/containers/HourlyWeatherContainer/HourlyWeatherContainer.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import FetchingWeather from '../../components/FetchingWeather/FetchingWeather';
+import chartType from '../../components/WeatherChart/chartType';
 import WeatherChart from '../../components/WeatherChart/WeatherChart';
 import { AppState } from '../../store/rootStore';
 import { IHourlyWeather } from '../../store/Weather/models/Weather';
@@ -21,14 +22,33 @@ const mapStateToProps = (state: AppState): LinkStateProps => {
     }
 }
 
+interface LocalState {
+    chartType: chartType;
+}
+
 class HourlyWeatherContainer extends Component<LinkProps> {
+
+    state: LocalState = {
+        chartType: chartType.TEMPERATURE
+    }
 
     fetchHourlyWeather = () => {
         let weatherChart = <FetchingWeather key={'fetching'} />;
         if (this.props.hourlyWeather) {
-            weatherChart = <WeatherChart hourlyWeather={this.props.hourlyWeather} />;
+            weatherChart = <WeatherChart
+                            hourlyWeather={this.props.hourlyWeather}
+                            chartType={(type: chartType) => this.onChartButtonClick(type)} />;
         }
         return weatherChart;
+    }
+
+    onChartButtonClick = (type: chartType) => {
+        console.log(type);
+        this.setState((prevState: LocalState) => {
+            return {
+                chartType: type
+            }
+        });
     }
 
     render() {
