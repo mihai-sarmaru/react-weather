36549eccc3c282b300accc1e11865fe7cc6b8335
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-12-14 19:09:14 +0200

    Animate current weather tempreture

--- src/components/CurrentWeather/CurrentWeather.tsx
+++ src/components/CurrentWeather/CurrentWeather.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import 'fontsource-roboto';
 import { Grid, Typography } from "@material-ui/core";
+import { Spring } from 'react-spring/renderprops';
 
 import WeatherIcon from '../WeatherIcon/WeatherIcon';
 
@@ -13,7 +14,7 @@ interface CurrentWeatherProps {
 
 const CurrentWeather: React.FC<CurrentWeatherProps> = (props) => {
     return (
-        <Grid container spacing={2} style={{marginBottom: '40px'}}>
+        <Grid container spacing={2} style={{marginBottom: '40px', width: '98%'}}>
             <Grid item xs={6}>
                 <div style={{textAlign: 'right'}}>
                     <WeatherIcon iconId={props.weatherIconId} size={140}/>
@@ -21,7 +22,11 @@ const CurrentWeather: React.FC<CurrentWeatherProps> = (props) => {
             </Grid>
             <Grid item xs={6}>
                 <div style={{textAlign: 'left'}}>
-                    <Typography variant='h2'>{props.temperature}°</Typography>
+                    <Spring
+                        from={{number: 0}}
+                        to={{number: props.temperature}}>
+                            {props => <Typography variant='h2'>{props.number.toFixed()}°</Typography> }
+                    </Spring>
                     <Typography variant='h6'>Feels like {props.feelsLike}°</Typography>
                     <Typography variant='subtitle1'>{props.description}</Typography>
                 </div>
