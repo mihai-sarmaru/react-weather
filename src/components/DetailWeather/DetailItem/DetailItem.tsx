bb607cb8addb257ed3ae53d5a91883c876c7a12c
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-12-18 11:14:03 +0200

    Add color to DetailItem icons

--- src/components/DetailWeather/DetailItem/DetailItem.tsx
+++ src/components/DetailWeather/DetailItem/DetailItem.tsx
@@ -17,52 +17,52 @@ const DetailItem: React.FC<DetailItemProps> = (props) => {
 
     switch (props.itemType) {
         case DetailItemType.PRECIPITATION_CHANCE:
-            itemIcon = <Icon.WiUmbrella size={42} />
+            itemIcon = <Icon.WiUmbrella size={42} color='#666' />
             itemValue = props.value + ' %';
             itemDescription = 'Chance';
             break;
         case DetailItemType.WIND:
-            itemIcon = <Icon.WiStrongWind size={42} />
+            itemIcon = <Icon.WiStrongWind size={42} color='#becbd1' />
             itemValue = props.value + ' km/h';
             itemDescription = 'Wind speed';
             break;
         case DetailItemType.UV_INDEX:
-            itemIcon = <Icon.WiSolarEclipse size={42} />
+            itemIcon = <Icon.WiSolarEclipse size={42} color='#cbad33' />
             itemValue = props.value as string;
             itemDescription = 'UV index';
             break;
         case DetailItemType.CLOUD_COVER:
-            itemIcon = <Icon.WiCloudRefresh size={42} />
+            itemIcon = <Icon.WiCloudRefresh size={42} color='#becbd1' />
             itemValue = props.value + ' %';
             itemDescription = 'Cloud cover';
             break;
         case DetailItemType.PRESSURE:
-            itemIcon = <Icon.WiBarometer size={42} />
+            itemIcon = <Icon.WiBarometer size={42} color='#666' />
             itemValue = props.value + ' mmHg';
             itemDescription = 'Pressure';
             break;
         case DetailItemType.HUMIDITY:
-            itemIcon = <Icon.WiHumidity size={42} />
+            itemIcon = <Icon.WiHumidity size={42} color='#86c3d6' />
             itemValue = props.value + ' %';
             itemDescription = 'Humidity';
             break;
         case DetailItemType.DEW_POINT:
-            itemIcon = <Icon.WiThermometerInternal size={42} />
+            itemIcon = <Icon.WiThermometerInternal size={42} color='#86c3d6' />
             itemValue = props.value + ' °';
             itemDescription = 'Dew point';
             break;
         case DetailItemType.VISIBILITY:
-            itemIcon = <Icon.WiTrain size={42} />
+            itemIcon = <Icon.WiTrain size={42} color='#999' />
             itemValue = props.value + ' km';
             itemDescription = 'Visibility';
             break;
         case DetailItemType.SUNRISE:
-            itemIcon = <Icon.WiSunrise size={42} />
+            itemIcon = <Icon.WiSunrise size={42} color='#edcf53' />
             itemValue = props.value as string;
             itemDescription = 'Sunrise';
             break;
         case DetailItemType.SUNSET:
-            itemIcon = <Icon.WiSunset size={42} />
+            itemIcon = <Icon.WiSunset size={42} color='#edcf53' />
             itemValue = props.value as string;
             itemDescription = 'Sunset';
             break;
