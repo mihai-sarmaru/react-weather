e2d3dc50a84d1ad2bddc41833d73aeeaf3064e0c
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-12-28 17:17:33 +0200

    Add localization to forecast list

--- src/utils/DateConverter.ts
+++ src/utils/DateConverter.ts
@@ -1,10 +1,12 @@
+import { ILocalization } from "../localization/model/localizationModel";
+
 export const UnixUTCToHourMinuteString = (utcTime: number) => {
     const date = new Date(utcTime);
     return date.getHours() + ':' + date.getMinutes();
 }
 
-export const UnixUTCToDayString = (utcTime: number) => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+export const UnixUTCToDayString = (utcTime: number, localization: ILocalization) => {
+    const days = localization.weekArray;
     const date = new Date(utcTime);
     return days[date.getDay()] + ' ' + date.getDate();
 }
