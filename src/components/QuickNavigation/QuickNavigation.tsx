96eeb12939913d4a4e3df643ea22989fdebdc7f1
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-12-28 14:41:30 +0200

    Use useState hook to handle Drawer toggle

--- src/components/QuickNavigation/QuickNavigation.tsx
+++ src/components/QuickNavigation/QuickNavigation.tsx
@@ -1,23 +1,41 @@
 import { Button, ButtonGroup } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as WiIcons from 'react-icons/wi';
+import * as MdIcons from 'react-icons/md';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../store/rootStore';
+import OptionsDrawer from '../OptionsDrawer/OptionsDrawer';
+
+interface QuickNavigationLocalState {
+    drawer: boolean;
+}
 
 const QuickNavigation = () => {
 
     const localization = useSelector((state: AppState) => state.localizationReducer.language);
+
+    const [navState, setNavState] = useState<QuickNavigationLocalState>({drawer: false});
+
+    const onOptionsButtonClick = () => {
+        console.log('entered')
+        setNavState({drawer: !navState.drawer});
+    }
     
     return(
-        <ButtonGroup variant='outlined' style={{margin: '40px 0'}}>
-            <Button startIcon={<WiIcons.WiThermometer />}
-                component={Link} to={'/'}>{localization.language.get('nav-now')}</Button>
-            <Button startIcon={<WiIcons.WiTime4 />}
-                component={Link} to={'/hourly'}>{localization.language.get('nav-hourly')}</Button>
-            <Button startIcon={<WiIcons.WiWindDeg />}
-                component={Link} to={'/forecast'}>{localization.language.get('nav-later')}</Button>
-        </ButtonGroup>
+        <React.Fragment>
+            <ButtonGroup variant='outlined' style={{margin: '40px 0'}}>
+                <Button startIcon={<WiIcons.WiThermometer />}
+                    component={Link} to={'/'}>{localization.language.get('nav-now')}</Button>
+                <Button startIcon={<WiIcons.WiTime4 />}
+                    component={Link} to={'/hourly'}>{localization.language.get('nav-hourly')}</Button>
+                <Button startIcon={<WiIcons.WiWindDeg />}
+                    component={Link} to={'/forecast'}>{localization.language.get('nav-later')}</Button>
+                <Button onClick={onOptionsButtonClick}><MdIcons.MdLanguage /></Button>
+            </ButtonGroup>
+            <OptionsDrawer open={navState.drawer} onClose={onOptionsButtonClick} />
+        </React.Fragment>
+        
     );
 }
 
