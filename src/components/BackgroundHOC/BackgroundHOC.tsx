6926da2af0fff9ef62934757e2bf25fc25d7ad8f
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2021-01-08 12:21:44 +0200

    Change top weather images based on current weather

--- src/components/BackgroundHOC/BackgroundHOC.tsx
+++ src/components/BackgroundHOC/BackgroundHOC.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { AppState } from '../../store/rootStore';
+import { getRandomWeatherImage } from '../../utils/ImageUtil';
 
 interface BackgroundHOCProps {}
 
 const BackgroundHOC: React.FC<BackgroundHOCProps> = (props) => {
-    const bkgImage = 'images/default.jpg';
+
+    const currentWeather = useSelector((state: AppState) => state.weatherReducer.weather);
+    console.log(currentWeather);
+
+    let weatherImage = '';
+    if (currentWeather.currentWeather !== undefined) {
+        weatherImage = getRandomWeatherImage(currentWeather.currentWeather.weather[0].id);
+    }
 
     return(
         <React.Fragment>
             <div className='fullscreen-image' style={{
-                    backgroundImage: `url(${bkgImage})`,
+                    backgroundImage: `url(${weatherImage})`,
                     backgroundPosition: 'center center',
                     backgroundSize: 'cover',
                     backgroundRepeat: 'no-repeat',
