00cbb56ceb3c71a560e2d2bc55363db90e1ffcc9
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2021-01-08 17:22:56 +0200

    Add sunny and cloudy animated loading icons

--- src/components/FetchingWeather/FetchingWeather.tsx
+++ src/components/FetchingWeather/FetchingWeather.tsx
@@ -1,14 +1,31 @@
 import React from "react";
-import { CircularProgress, Typography } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import { AppState } from "../../store/rootStore";
+import { Sunny, Cloudy } from 'weather-styled-icon';
 
 const FetchingWeather = () => {
 
     const localization = useSelector((state: AppState) => state.localizationReducer.language);
 
-    return <div style={{marginTop: '300px'}}>
-        <CircularProgress />
+    const getLoadingIcon = () => {
+        let loadingIcon = <Sunny />;
+        switch (Math.floor(Math.random() * Math.floor(2))) {
+            case 0:
+                loadingIcon = <Sunny />;
+                break;
+            case 1:
+                loadingIcon = <Cloudy />;
+                break;
+            default:
+                loadingIcon = <Sunny />;
+                break;
+        }
+        return loadingIcon;
+    }
+
+    return <div style={{marginTop: '250px'}}>
+        {getLoadingIcon()}
         <Typography variant='subtitle1'>{localization.language.get('loading')}</Typography>
     </div>;
 };
