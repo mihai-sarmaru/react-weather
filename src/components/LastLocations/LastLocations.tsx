5faf4aac046eea04c3b4bcc6a8b68b7a059e2851
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2021-01-11 15:49:47 +0200

    Add location icons to last searched locations

--- src/components/LastLocations/LastLocations.tsx
+++ src/components/LastLocations/LastLocations.tsx
@@ -1,4 +1,6 @@
+import { Icon } from '@material-ui/core';
 import React from 'react';
+import * as Icons from 'react-icons/md';
 import { getLocationsFromStorage } from '../../utils/LocationUtils';
 
 const LastLocations = () => {
@@ -6,9 +8,12 @@ const LastLocations = () => {
     const locations = getLocationsFromStorage();
 
     return(
-        <div style={{textAlign: 'left', paddingLeft: '20px'}}>
+        <div style={{textAlign: 'left', padding: '10px 20px'}}>
             {locations === null ? null : locations.map((loc, index) => {
-                return <p key={index}>{loc.name}</p>
+                return <div style={{display: 'flex', justifyContent: 'left', alignItems: 'center'}}>
+                        <Icons.MdLocationOn className='typography-secondary' /> 
+                        <p style={{margin: '10px 0 10px 5px'}} key={index}>{loc.name}</p>
+                    </div>
             })}
         </div>
     );
