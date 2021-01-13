05ecac5f6bbfa5ff852fab8287ff5f89226e7901
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2021-01-13 14:20:54 +0200

    Set Switch component checked prop to local state

--- src/components/BackgroundImageToggle/BackgroundImageToggle.tsx
+++ src/components/BackgroundImageToggle/BackgroundImageToggle.tsx
@@ -1,11 +1,18 @@
+import React, { useEffect, useState } from 'react';
 import { Typography, Grid, Switch } from '@material-ui/core';
-import React from 'react';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../store/rootStore';
+import { getUnsplashOption } from '../../utils/ImageUtil';
 
 const BackgroundImageToggle = () => {
 
     const localization = useSelector((state: AppState) => state.localizationReducer.language);
+
+    const [option, setOption] = useState(false);
+
+    useEffect(() => {
+        setOption(getUnsplashOption());
+    }, []);
 
     return(
         <div style={{margin: '10px 0', padding: '0 20px', width: '90%'}}>
@@ -14,7 +21,7 @@ const BackgroundImageToggle = () => {
                     <Typography variant='body1'>{localization.language.get('unsplash')}</Typography>
                 </Grid>
                 <Grid item xs={3}>
-                    <Switch color='secondary' />
+                    <Switch color='secondary' checked={option} />
                 </Grid>
             </Grid>
         </div>
